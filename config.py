278c4c5cf9332b9707fcf88b4aa8efc4bd74fb74
Utekhin <utekhin@yandex.ru>
2020-01-07 14:46:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ GIPHY_API_KEY = 'tM7IdU36TzQHajDyWx4FyTgJ9DfkArIR'
 BOT_API_TOKEN = '1044936068:AAGFz8aOzWIzHRCweJfQt0QvwHPdVI8sVS0'
 
 # Text blocks
-WELCOME_TETX = "Hi there!\nI'm catlover bot\nВведите любое сообщение для получения gif с котами или воспользуйтесь кнопками в меню."
+WELCOME_TETX = "Hi there!\nI'm catlover bot\nUse buttons or type anything to get your cats."
 RENDER_TEXT = "Input your text to place it over random cat pic for your gif"
 RENDER_TEXT_WAIT = 'Wait while the cats are preparing your a gif for you'
 ABOUT_TEXT = 'Get a cat gif using cataas.com.'
