895dca88af01d80ed351ae2d7875e3a0ddfae6b3
Utekhin <utekhin@yandex.ru>
2020-01-07 14:44:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_PROXY_URL = 'http://146.185.192.26:3128'
 GIPHY_API_KEY = 'tM7IdU36TzQHajDyWx4FyTgJ9DfkArIR'
 
 # It's wrong token :)
-BOT_API_TOKEN = '757367620:AAG-hjldVvwiQhcED7FdrH-U4eE2tMsL7ng'
+BOT_API_TOKEN = '1044936068:AAGFz8aOzWIzHRCweJfQt0QvwHPdVI8sVS0'
 
 # Text blocks
 WELCOME_TETX = "Hi there!\nI'm catlover bot\nВведите любое сообщение для получения gif с котами или воспользуйтесь кнопками в меню."
