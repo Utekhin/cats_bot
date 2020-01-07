0a9cf6285cb2c8460c6c6513f0f0c8f482f9a0db
Utekhin <utekhin@yandex.ru>
2020-01-07 15:04:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# HTTP proxy url, uncomment if you need proxy
-#BOT_PROXY_URL = 'http://146.185.192.26:3128'
-
 # Api key for developers giphy api
 GIPHY_API_KEY = 'tM7IdU36TzQHajDyWx4FyTgJ9DfkArIR'
 
@@ -8,7 +5,7 @@ GIPHY_API_KEY = 'tM7IdU36TzQHajDyWx4FyTgJ9DfkArIR'
 BOT_API_TOKEN = '1044936068:AAGFz8aOzWIzHRCweJfQt0QvwHPdVI8sVS0'
 
 # Text blocks
-WELCOME_TETX = "Hi there!\nI'm catlover bot\nUse buttons or type anything to get your cats"
+WELCOME_TEXT = "Hi there!\nI'm catlover bot\nUse buttons or type anything to get your cats"
 RENDER_TEXT = "Input your text to place it over random cat pic for your gif"
 RENDER_TEXT_WAIT = 'Wait while the cats are preparing your a gif for you'
 ABOUT_TEXT = 'Get a cat gif using cataas.com'
